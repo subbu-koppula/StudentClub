e05237163b889c45843b10e159695e1031c02e3b
saisu-sai <saikoppulajee@gmail.com>
2024-12-20 08:21:52 +0530

    uploading current site

---
+++ src/app/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage: React.FC = () => {
+    return (
+        <div style={{ textAlign: 'center', marginTop: '50px' }}>
+            <h1>Welcome to the Student Club</h1>
+            <p>Join us to explore new opportunities and make new friends!</p>
+        </div>
+    );
+};
+
+export default HomePage;
